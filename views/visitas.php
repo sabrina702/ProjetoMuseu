349704e9509585bb933c7b8d98b89cd9f42389e3
sabrina702 <sabrinassgs101@gmail.com>
2025-06-27 15:55:13 -0300

    feat: 5 Páginas Visitas e Agendamento

---
+++ views/visitas.php
@@ -0,0 +1,50 @@
+<?php
+$pagina_css = 'visitas.css';
+require "includes/header.php";
+?>
+
+<main class="container my-5 position-relative pt-5">
+  <div class="section-label">
+    VISITAS
+    <div class="underline"></div>
+  </div>
+
+  <div class="content mt-4" id="conteudo">
+    <section class="acervo">
+      <div class="grid-acervo">
+        <div class="item-acervo">
+          <img src="/SitedoMuseu/static/imagens/temporaria.png" alt="Ambiente da Caatinga">
+          <div class="info">
+            <p class="tag">ambiente</p>
+            <h3>Caatinga</h3>
+          </div>
+        </div>
+        <div class="item-acervo">
+          <img src="/SitedoMuseu/static/imagens/temporaria.png" alt="Ambiente do Cerrado">
+          <div class="info">
+            <p class="tag">ambiente</p>
+            <h3>Cerrado</h3>
+          </div>
+        </div>
+        <div class="item-acervo">
+          <img src="/SitedoMuseu/static/imagens/temporaria.png" alt="Ambiente da Amazônia">
+          <div class="info">
+            <p class="tag">ambiente</p>
+            <h3>Mata Atântica</h3>
+          </div>
+        </div>
+        <div class="item-acervo">
+          <img src="/SitedoMuseu/static/imagens/temporaria.png" alt="Ambiente Interno do Museu">
+          <div class="info">
+            <p class="tag">ambiente</p>
+            <h3>Ecossitema Marinho</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div>
+      <h2 class="titulo">Guia de Visitas</h2>
+      <p class="texto-guia">Aqui você encontrará as informações sobre como funcionam as visitas ao museu, incluindo horários, informações sobre os ambientes e muito mais. Aproveite a nossa trilha de conhecimento!</p>
+    </div>
+</main>
