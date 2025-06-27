f4b427f7e8de91b6a8cf53c613cdda21d592f197
emily-180 <emily.moreira.8@gmail.com>
2025-06-27 16:06:51 -0300

    feat:6 adicionando a página contato e API

---
+++ views/contato.php
@@ -0,0 +1,42 @@
+<?php
+require_once __DIR__ . '/../chave.env.php';
+
+$pagina_css='contato.css';
+require "includes/header.php";
+?>
+    <main class="container my-5 position-relative pt-5">
+        <div class="section-label">
+          CONTATO
+          <div class="underline"></div>
+        </div>
+      
+        <div class="content mt-4" id="conteudo">
+            <h2>Informações de contato</h2>
+                <div class="grid-contato">
+                    <div class="informacoes-contato">
+                        <p><strong>Email:</strong> contato@museuciencias.org</p>
+                        <p><strong>Telefone:</strong> (00) 1234-5678</p>
+                        <p><strong>Instagram:</strong> <a href="https://instagram.com/museuciencias">instagram.com/museuciencias</a></p>
+                        <p><strong>Localização:</strong> Rua do Conhecimento, 123 - Centro, Cidade/Estado</p>
+                    </div>
+                    
+                    <div class="mapa-container">
+                        <iframe
+                            id="gmp-map"
+                            class="gmap"
+                            frameborder="0"
+                            style="border: 0;"
+                            src="https://www.google.com/maps/embed/v1/place?key=<?php echo $google_maps_key; ?>&q=IFSULDEMINAS+-+Campus+Machado&zoom=15"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+
+                </div>
+        </div>
+    </main>
+    <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo $google_maps_key; ?>&callback=initMap" async defer></script>
+
+   
+<?php
+require "includes/footer.php";
+?>
--- views/includes/header.php
+++ views/includes/header.php
@@ -35,7 +35,7 @@
                 <a href="/ProjetoMuseu/views/sobre.php" class="nav-link-custom">SOBRE</a>
                 <a href="/ProjetoMuseu/template/visitas.php" class="nav-link-custom">VISITAS</a>
                 <a href="/ProjetoMuseu/template/agendamento.php" class="nav-link-custom">AGENDAMENTO</a>
-                <a href="/ProjetoMuseu/template/contato.html" class="nav-link-custom">CONTATO</a>
+                <a href="/ProjetoMuseu/views/contato.php" class="nav-link-custom">CONTATO</a>
             </div>
         </nav>
     </header>
